74becc6cd179f6f4f7e25b3e499d295a9dc6f9f3
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2025-01-02 22:08:22 -0300

    feat: improve the UI of the login page

--- src/app/features/login/containers/root-login/root-login.component.ts
+++ src/app/features/login/containers/root-login/root-login.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Button } from 'primeng/button';
+import { Panel } from 'primeng/panel';
 
 @Component({
   selector: 'app-root-login',
   standalone: true,
-  imports: [Button],
-  template: `<p-button (onClick)="auth.loginWithRedirect()">Login</p-button>`,
+  imports: [Button, Panel],
+  templateUrl: './root-login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './root-login.component.scss',
 })
 export class RootLoginComponent {
   protected readonly auth = inject(AuthService);

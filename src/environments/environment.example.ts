0a7de176a41964ed85ada3fd19af8f513bdfaa8b
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2025-01-05 12:24:27 -0300

    refactor: replace public environment.ts file with environment.example.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  auth0: {
+    domain: 'replace',
+    clientId: 'replace',
+    useRefreshTokens: true,
+    useRefreshTokensFallback: true,
+
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      audience: 'replace',
+    },
+
+    httpInterceptor: {
+      allowedList: ['http://localhost:3000/*'],
+    },
+  },
+  apiUrl: 'http://localhost:3000',
+};

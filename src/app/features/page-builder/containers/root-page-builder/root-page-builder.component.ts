1bf64c20bf75bf70e4b7db0f9e246acefe9884f8
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2025-01-05 15:28:47 -0300

    refactor: decrease the paddings to 16px

--- src/app/features/page-builder/containers/root-page-builder/root-page-builder.component.ts
+++ src/app/features/page-builder/containers/root-page-builder/root-page-builder.component.ts
@@ -1,23 +1,25 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { Button } from 'primeng/button';
 import { Toolbar } from 'primeng/toolbar';
+import { map } from 'rxjs';
 import { EditorComponent } from '../../components/editor/editor.component';
 
 @Component({
   selector: 'app-root-page-builder',
   standalone: true,
-  imports: [RouterModule, Toolbar, EditorComponent, Button],
+  imports: [RouterModule, Toolbar, EditorComponent, Button, AsyncPipe],
   templateUrl: './root-page-builder.component.html',
   styles: [
     `
       :host {
-        padding: 24px;
+        padding: 16px;
         padding-bottom: 0;
         display: grid;
         grid-template-rows: auto 1fr;
-        gap: 24px;
+        gap: 16px;
         min-height: 100vh;
 
         @media (min-width: 500px) {
@@ -28,4 +30,6 @@ import { EditorComponent } from '../../components/editor/editor.component';
 })
 export class RootPageBuilderComponent {
   protected auth = inject(AuthService);
+
+  readonly username$ = this.auth.user$.pipe(map((user) => user?.nickname));
 }
